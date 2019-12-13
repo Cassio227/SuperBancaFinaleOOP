2d6013e711027074daff2e8c1d5339d60cb902a6
cl458027 <cl458027@lib-084>
2019-12-13 14:28:13 +0100

    ASDASDSA

---
+++ src/HomeBankingGUI.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HomeBankingGUI extends JFrame {
+	private JPanel pannelloLogin;
+	private JPanel pannelloGestione;
+	
+	public HomeBankingGUI(){
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(600,400);
+		
+	}
+	
+	
+	
+}
